f0eb4abe0a7efcbf831805ad5a4de8a443cbed66
Logan <amerjabar0000@gmail.com>
2022-01-08 14:22:55 +0300

    Adjusted some functionality, added pause and resume features.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const requestUrl = 'http://localhost:6500/api/shows/1/trailer/upload-request';
-const uploadUrl = 'http://localhost:6500/api/shows/1/trailer/upload';
+const requestUrl = 'http://localhost:6500/api/shows/something17/trailer/upload-request';
+const uploadUrl = 'http://localhost:6500/api/shows/something17/trailer/upload';
 const fileType = 'jpg'
-const CHUNK_SIZE = 50;
+const CHUNK_SIZE = 100;
 
 const uploadInput = document.querySelector('#upload-input');
 const uploadHandler = document.querySelector('#upload-handler');
@@ -10,64 +10,58 @@ const uploadPauseHandler = document.querySelector('#upload-pause-handler');
 const uploadResumeHandler = document.querySelector('#upload-resume-handler');
 
 const fileReader = new FileReader();
+let pauseUpload = false;
 
 uploadHandler.addEventListener('click', () => {
+    pauseUpload = false;
     const file = uploadInput.files[0];
     fileReader.onload = uploadFile(file)
-    
+
     fileReader.readAsArrayBuffer(file);
 })
 
 const uploadFile = async (file) => {
+
     let iterations = file.size / CHUNK_SIZE;
     const uploadedChunkSize = await getUploadedChunkSize();
     
     console.log(uploadedChunkSize, (file.size));
+    console.log(
+        uploadedChunkSize == 0
+        ? 'Uploading from scratch'
+        : uploadedChunkSize < file.size
+        ? 'Continuing uploading file'
+        : 'File is uploaded'
+    );
+    iterations = (file.size - uploadedChunkSize) / CHUNK_SIZE;
 
-    if ( uploadedChunkSize == 0 ) {
-        console.log('uploading from scratch.');
-        for ( let i = 0; i < iterations; i++ ) {
-            const currentChunk = file.slice(CHUNK_SIZE * i, CHUNK_SIZE * (i + 1));
-            await fetch(uploadUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Length': file.size,
-                    'Content-Type': 'application/octet-stream',
-                    'File-Type': fileType
-                },
-                body: currentChunk
-            })
-    
-            progressIndicator.innerText = `${Math.round(((i * CHUNK_SIZE) / file.size) * 100)}%`;
-        } 
-        
-    } else if ( uploadedChunkSize < file.size ) {
-        console.log('continuing uploading.');
-        iterations = (file.size - uploadedChunkSize) / CHUNK_SIZE;
+    for ( let i = 0; i < iterations; i++ ) {
 
-        for ( let i = 0; i < iterations; i++ ) {
-            const currentChunk = file.slice(
-                uploadedChunkSize + (CHUNK_SIZE * i),
-                uploadedChunkSize + CHUNK_SIZE * (i + 1)
-            );
-            await fetch(uploadUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Length': file.size,
-                    'Content-Type': 'application/octet-stream',
-                    'File-Type': fileType      
-                },
-                body: currentChunk
-            })
-    
-            progressIndicator.innerText = `${Math.round(((i * CHUNK_SIZE) / file.size) * 100)}%`;
-        }
+        if ( pauseUpload )
+            return;
 
-    } else {
-        console.log('its fucking uploaded.')
+        const currentChunk = file.slice(
+            uploadedChunkSize + (CHUNK_SIZE * i),
+            uploadedChunkSize + CHUNK_SIZE * (i + 1)
+        );
+
+        const response = await fetch(uploadUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Length': file.size,
+                'Content-Type': 'application/octet-stream',
+                'File-Type': fileType
+            },
+            body: currentChunk
+        })
+        const { currentUploadedChunkSize } = await response.json();
+
+        progressIndicator.innerText = `${Math.round((currentUploadedChunkSize / file.size) * 100)}%`;
     }
 
 }
+
+uploadPauseHandler.addEventListener('click', () => pauseUpload = true)
 
 const getUploadedChunkSize = async () => {
     try {
